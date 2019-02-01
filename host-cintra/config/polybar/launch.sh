2d4c4dfb1d7fa2736e54eec078e6e4ec2a99921b
Taylor Thurlow <taylorthurlow8@gmail.com>
2019-02-01 12:57:25 -0800

    Change cintra config from linux to macos

--- host-cintra/config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-killall -q polybar
-
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-polybar -r cintra-center &
-polybar -r cintra-left &
-polybar -r cintra-right &
