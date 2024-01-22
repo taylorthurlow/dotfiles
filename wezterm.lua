80e5030e1bb5707744af7fada2122b551c752732
Taylor Thurlow <thurlow@hey.com>
2024-01-22 15:43:45 -0800

    Format wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,14 +7,14 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 config.automatically_reload_config = true
-config.color_scheme = 'Ayu Mirage'
-config.front_end = 'WebGpu'
+config.color_scheme = "Ayu Mirage"
+config.front_end = "WebGpu"
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
 config.initial_rows = 40
@@ -22,10 +22,10 @@ config.initial_cols = 120
 
 -- Text rendering
 config.font_size = 14.0
-config.font = wezterm.font('CommitMono400w')
+config.font = wezterm.font("CommitMono400w")
 config.line_height = 1.2
-config.freetype_load_target = 'Light'
-config.default_prog = { '/bin/zsh', '-lc', 'zellij' }
+config.freetype_load_target = "Light"
+config.default_prog = { "/bin/zsh", "-lc", "zellij" }
 
 -- Speed up ^ and ~ processing
 config.use_dead_keys = false
@@ -39,18 +39,18 @@ config.disable_default_mouse_bindings = true
 
 local act = wezterm.action
 config.keys = {
-  { key = 'k', mods = 'CTRL', action = act.ClearScrollback 'ScrollbackOnly' },
+	{ key = "k", mods = "CTRL", action = act.ClearScrollback("ScrollbackOnly") },
 
-  { key = 'q', mods = 'SUPER', action = act.QuitApplication },
-  { key = 'n', mods = 'SUPER', action = act.SpawnWindow },
-  { key = 'h', mods = 'SUPER', action = act.HideApplication },
-  { key = 'r', mods = 'SUPER', action = act.ReloadConfiguration },
-  { key = 'v', mods = 'SUPER', action = act.PasteFrom 'Clipboard' },
-  { key = '0', mods = 'SUPER', action = act.ResetFontSize },
-  { key = '=', mods = 'SUPER', action = act.IncreaseFontSize },
-  { key = '-', mods = 'SUPER', action = act.DecreaseFontSize },
+	{ key = "q", mods = "SUPER", action = act.QuitApplication },
+	{ key = "n", mods = "SUPER", action = act.SpawnWindow },
+	{ key = "h", mods = "SUPER", action = act.HideApplication },
+	{ key = "r", mods = "SUPER", action = act.ReloadConfiguration },
+	{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+	{ key = "0", mods = "SUPER", action = act.ResetFontSize },
+	{ key = "=", mods = "SUPER", action = act.IncreaseFontSize },
+	{ key = "-", mods = "SUPER", action = act.DecreaseFontSize },
 
-  { key = 'p', mods = 'SUPER|SHIFT', action = act.ActivateCommandPalette },
+	{ key = "p", mods = "SUPER|SHIFT", action = act.ActivateCommandPalette },
 }
 
 -- and finally, return the configuration to wezterm
