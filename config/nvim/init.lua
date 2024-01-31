c756eb870e28de3ce02091888759dc87f836c82d
Taylor Thurlow <thurlow@hey.com>
2024-01-30 22:23:35 -0800

    Add jq to conform

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -487,6 +487,7 @@ lspconfig.tsserver.setup({})
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
+		json = { "jq" },
 		-- python = { "isort", "black" },
 		-- Use a sub-list to run only the first available formatter
 	},
