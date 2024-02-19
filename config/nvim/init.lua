529993c1d7518bf123deb0164dd7daf196496609
Taylor Thurlow <thurlow@hey.com>
2024-02-19 09:03:58 -0800

    Add auto-dark-mode

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -31,6 +31,20 @@ require("lazy").setup({
 	"dhruvasagar/vim-table-mode", -- Table editing
 
 	{
+		"f-person/auto-dark-mode.nvim",
+		config = {
+			update_interval = 1000,
+			set_dark_mode = function()
+				vim.api.nvim_set_option("background", "dark")
+				vim.cmd("colorscheme ayu")
+			end,
+			set_light_mode = function()
+				vim.api.nvim_set_option("background", "light")
+				vim.cmd("colorscheme ayu")
+			end,
+		},
+	},
+	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
